bf5c5cc8a28acee7931e2fb5344fbfd610688104
Judith <giraltjudith@gmail.com>
2022-02-09 19:16:26 +0100

    MapReduce optimized

--- src/mapreduce/MapReduce.java
+++ src/mapreduce/MapReduce.java
@@ -3,14 +3,12 @@ package mapreduce;
 import dataframe.DataFrame;
 
 import java.util.List;
-import java.util.Optional;
+import java.util.Objects;
 import java.util.function.BinaryOperator;
 import java.util.function.Function;
 import java.util.function.Predicate;
 
 public class MapReduce {
-
-    private static final Object T = 0;
 
     /**
      * Returns a value following the MapReduce computing design pattern of a list of Dataframes
@@ -22,6 +20,6 @@ public class MapReduce {
      * @return  The added value of computing, after applying the reduction
      */
     public static <T> String mapRed (List<DataFrame> df, Predicate<DataFrame> predicate, Function<DataFrame, T> function, BinaryOperator<T> op) {
-        return df.stream().filter(predicate).map(function).reduce((T) T, op).toString();
+        return Objects.requireNonNull(df.stream().filter(predicate).map(function).reduce(op).orElse(null)).toString();
     }
 }
--- src/mapreduce/test/MapReduceJUnitTest.java
+++ src/mapreduce/test/MapReduceJUnitTest.java
@@ -9,40 +9,38 @@ import org.junit.jupiter.api.Test;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 public class MapReduceJUnitTest {
 
-    DataFrameAPI df1, df2, df3;
     List<DataFrame> list = new ArrayList<>();
+    DataFrameAPI df1,df2,df3;
 
-    {
-        try {
-            df1 = new DataFrameAPI(".\\src\\api\\APIFiles\\cities.csv","csv");
-            df2 = new DataFrameAPI(".\\src\\api\\APIFiles\\cities.json","json");
-            df3 = new DataFrameAPI(".\\src\\api\\APIFiles\\cities.txt","txt");
-            list.add(df1);
-            list.add(df2);
-            list.add(df3);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    { try {
+        df1 = new DataFrameAPI(".\\src\\composite\\EU\\Spain\\Catalonia.csv","csv");
+        df2 = new DataFrameAPI(".\\src\\composite\\EU\\Spain\\Galicia.csv","csv");
+        df3 = new DataFrameAPI(".\\src\\composite\\EU\\Germany.json","json");
+
+        list.add(df1);
+        list.add(df2);
+        list.add(df3);
+
+    } catch (IOException e) { e.printStackTrace(); }
     }
 
     @Test
     public void testSumSizeList() {
-        String sum = "384";
+        String sum = "12";
         System.out.println("Testing SumSizeList...");
-        String sum2 = MapReduce.mapRed(list, x -> x.getList().get(0).get("LatD").equals(48.0), f -> (double) f.size(), Double::sum);
+        String sum2 = MapReduce.mapRed(list, x -> x.size()>3, DataFrame::size, Integer::sum);
         Assertions.assertEquals(sum, sum2);
-        System.out.println(sum + " | " + MapReduce.mapRed(list, x -> x.getList().get(0).get("LatD").equals(48.0), f -> (double) f.size(), Double::sum));
+        System.out.println(sum + " | " + MapReduce.mapRed(list, x -> x.size()>3, DataFrame::size, Integer::sum));
     }
 
     @Test
     public void testMinValue() {
-        String min = "0";
+        String min = "4";
         System.out.println("Testing MinValue...");
-        String min2 = MapReduce.mapRed(list, x -> x.getList().get(0).get("LatD").equals(48.0), f -> (double) f.size(), Double::min);
+        String min2 = MapReduce.mapRed(list, x -> x.size()>3, DataFrame::size, Integer::min);
         Assertions.assertEquals(min, min2);
         System.out.println(min + " | " + min2);
 
@@ -52,9 +50,17 @@ public class MapReduceJUnitTest {
     public void testMaxColumnsList() {
         String max = "10";
         System.out.println("Testing MaxColumnsList...");
-        Assertions.assertEquals(max, MapReduce.mapRed(list, x -> x.getList().get(0).get("LatD").equals(48.0), f -> (double) f.columns(), Double::max));
-        System.out.println(max + " | " + MapReduce.mapRed(list,x -> x.getList().get(0).get("LatD").equals(48.0), f -> (double) f.columns(), Double::max));
+        Assertions.assertEquals(max, MapReduce.mapRed(list, x -> x.columns()>9, DataFrame::columns, Integer::max));
+        System.out.println(max + " | " + MapReduce.mapRed(list,x -> x.columns()>9, DataFrame::columns, Integer::max));
 
+    }
+
+    @Test
+    public void testCity() {
+        String city = "BarcelonaSantiago de CompostelaHamburg";
+        System.out.println("Testing City...");
+        Assertions.assertEquals(city, MapReduce.mapRed(list, x -> x.columns()>9, f -> f.at(0, "City"), String::concat));
+        System.out.println(city + " | " + MapReduce.mapRed(list, x -> x.columns()>9, f -> f.at(0, "City"), String::concat));
     }
 
 }
